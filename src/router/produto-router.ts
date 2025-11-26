b58bd889a1da6bf102a7438184dc87c11c1d6f75
Luis Henrique Ries <prof.ries@gmail.com>
2025-11-26 20:43:06 -0300

    Trabalhando com imagens

--- src/router/produto-router.ts
+++ src/router/produto-router.ts
@@ -1,5 +1,19 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { ProdutoController } from "../controller/produto-controller";
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './my-uploads')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix + "."+file.originalname.substring(file.originalname.length-3, file.originalname.length))
+  }
+})
+
+//const upload = multer({dest:'./uploads/'});
+const upload = multer({storage:storage});
 
 // /api/produtos
 export const produtoRotas = (controller: ProdutoController): Router => {
@@ -10,6 +24,11 @@ export const produtoRotas = (controller: ProdutoController): Router => {
     router.get('/:id', controller.buscarPorId);
     router.put('/:id', controller.atualizar );
     router.delete('/:id', controller.deletar );
+
+    router.post('/imagens/upload', upload.single('imagem') , async (req: Request, res: Response): Promise<any>  => {
+        console.log(req.file);
+        res.send("Imagem carregada com sucesso!");
+    });
 
     return router;
 }
